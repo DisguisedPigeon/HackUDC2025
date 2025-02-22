2e2c0c0e8d185a15df4e613ae882e67827aa9d20
alexborrazasm <alexborrazasm@gmail.com>
2025-02-22 23:16:53 +0100

    bug 2

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi import Request
 from pydantic import BaseModel, HttpUrl
 import httpx
 from dotenv import load_dotenv
